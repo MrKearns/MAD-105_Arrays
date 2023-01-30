9f338ca27d9bb87101e1a64c0eea8b6372604c4a
MrKearns <jkearns631@students.mchenry.edu>
2023-01-30 09:51:24 -0600

    array of coffee supplies

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var coffeeSupplies = arrayListOf("Coffee Grounds", "Creamer", "Sugar", "Coffee Filters", "Takeaway Cups", "Takeaway Lids","Cup Sleeves", "Napkins" )
+
+    println("Size: ${coffeeSupplies.size}\n")
+
+    for (i in coffeeSupplies){
+        println(i)
+    }
+
+
+
 }
